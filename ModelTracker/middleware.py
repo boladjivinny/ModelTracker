6ed49ff71c9628b4807074f44ce3794fce4b8634
Vinny <23731062+boladjivinny@users.noreply.github.com>
2021-11-07 11:27:57 +0100

    Update middleware.py

--- ModelTracker/middleware.py
+++ ModelTracker/middleware.py
@@ -2,7 +2,7 @@ from ModelTracker.Tracker import ModelTracker
 class ModelTrackerMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
-    def process_request(self, request):
+    def __call__(self, request):
         ModelTracker.thread.request = request
 
 
